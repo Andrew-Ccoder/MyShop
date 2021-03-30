a50cc228d53bbdf31822c2e73f1f87bfcaa46991
Andrew.Ccoder <Andrewyacoub95@gmail.com>
2021-03-30 23:15:24 +0200

    Making Product category Repository and Model

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductCategoryRepository.cs
@@ -0,0 +1,83 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    class ProductCategoryRepository
+    {
+        ObjectCache Cache = MemoryCache.Default;
+        List<ProductCategory> productCategories = new List<ProductCategory>();
+
+        public ProductCategoryRepository()
+        {
+            productCategories = Cache["productCategories"] as List<ProductCategory>;
+
+            if (productCategories == null)
+            {
+                productCategories = new List<ProductCategory>();
+            }
+        }
+
+        public void commit()
+        {
+            Cache["productCategories"] = productCategories;
+        }
+
+        public void Insert(ProductCategory p)
+        {
+            productCategories.Add(p);
+        }
+
+        public void Update(ProductCategory productCategory)
+        {
+            ProductCategory ProductCategoryToUpdate = productCategories.Find(p => p.Id == productCategory.Id);
+
+            if (ProductCategoryToUpdate != null)
+            {
+                ProductCategoryToUpdate = productCategory;
+            }
+            else
+            {
+                throw new Exception("The product Category not found");
+            }
+        }
+
+        public ProductCategory Find(string Id)
+        {
+            ProductCategory productCategory = productCategories.Find(p => p.Id == Id);
+
+            if (productCategory != null)
+            {
+                return productCategory;
+            }
+            else
+            {
+                throw new Exception("The product Category not found");
+            }
+        }
+
+        public IQueryable<ProductCategory> Collection()
+        {
+            return productCategories.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            ProductCategory productCategoryToDelete = productCategories.Find(p => p.Id == Id);
+
+            if (productCategoryToDelete != null)
+            {
+                productCategories.Remove(productCategoryToDelete);
+            }
+            else
+            {
+                throw new Exception("The product Category not found");
+            }
+        }
+    }
+}
